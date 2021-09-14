74950b3b3d06fb2b2f4b12a9aae2231b3bbd7e4a
Andrey Hurpia da Rocha <ahrocha@gmail.com>
2021-09-14 19:33:20 -0300

    short code processing removed
    Short codes are being processed at the backend.

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,27 +5,19 @@ class Post extends Component {
 
     constructor() {
         super();
-        console.log("construtor");
-        console.log(this);
         this.state = {Post: [], loading: true, title: "Andrey", text: "Hurpia da Rocha"};
     }
     componentDidMount() {
-        console.log("didmount");
-        console.log(this);
-        console.log(this.props.match.params.id);
         this.getPost(this.props.match.params.id);
     }
     
     getPost(id) {
        axios.get("https://node.hurpia.com.br/post/" + id).then(Post => {
-        console.log(Post.data);
         this.setState({ Post: Post.data, loading: false})
        })
     }
 
     render() {
-        console.log("render");
-        console.log(this);
         const loading = this.state.loading;
         if (loading === true) {
             return (
@@ -35,10 +27,7 @@ class Post extends Component {
             );
         } else {
             const item = this.state.Post;
-            console.log(item);
             let post_content = item.post_content.replace(/\n/g, "<br />");
-            post_content = post_content.replace(/\[(.*?)\]/g, "");
-            console.log(post_content);
             const postsList = (
                 <div>
                     <h2>{item.post_title}</h2>
